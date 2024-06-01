16393e876028453c9f05c7a062bb4ac84265d704
Hady <hady_awayda@hotmail.com>
2024-06-01 23:58:18 +0300

    Added reverseOdd

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -41,3 +41,13 @@ function oddishOrEvenish(num) {
 
   return sum % 2 === 0 ? "Evenish" : "Oddish";
 }
+
+function reverseOdd(str) {
+  let arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length % 2 !== 0) {
+      arr[i] = arr[i].split("").reverse().join("");
+    }
+  }
+  return arr.join(" ");
+}
