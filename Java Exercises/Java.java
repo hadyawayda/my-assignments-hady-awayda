4f967db580b1ddda7be0df1fd0d09a375351e873
Hady <hady_awayda@hotmail.com>
2024-06-09 14:37:41 +0300

    Added toArray

--- Java Exercises/Java.java
+++ Java Exercises/Java.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Java {
 	public static void main(String[] args) {
-		Boolean[] arr = { true, false, true, true, false };
-		System.out.println(countTrue(arr));
+		Map<String, Object> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        System.out.println(Arrays.deepToString(toArray(map)));
 	}
 	public static int countTrue(Boolean[] arr) {
 		int count = 0;
@@ -12,4 +16,23 @@ public class Java {
 		}
 		return count;
 	}
+	
+	public static Object[][] toArray(Map<String, Object> map) {
+        if (map.isEmpty()) {
+            return new Object[0][0];
+        }
+
+        Object[][] array = new Object[map.size()][2];
+        int index = 0;
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            array[index][0] = entry.getKey();
+            array[index][1] = entry.getValue();
+            index++;
+        }
+
+        return array;
+    }
+
+	
 }
