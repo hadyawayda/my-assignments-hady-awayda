b27ece2bc15c4aca95e25936c5465ac6b0dbc693
Hady <hady_awayda@hotmail.com>
2024-06-09 15:45:55 +0300

    Added all functions

--- Java Exercises/Java.java
+++ Java Exercises/Java.java
@@ -1,12 +1,6 @@
 import java.util.*;
 
 public class Java {
-	public static void main(String[] args) {
-		Map<String, Object> map = new HashMap<>();
-        map.put("a", 1);
-        map.put("b", 2);
-        System.out.println(Arrays.deepToString(toArray(map)));
-	}
 	public static int countTrue(Boolean[] arr) {
 		int count = 0;
 		for (Boolean i : arr) {
@@ -34,5 +28,64 @@ public class Java {
         return array;
     }
 
-	
+	public static String luckyNumbers(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (String.valueOf(arr[i]).contains("7")) {
+				return "LUCKY";
+			}
+		}
+		return "there is no 7 in the array :(";
+	}
+
+	public static String oddishOrEvenish(int num) {
+		int sum = 0;
+		for (int i = 0; i < Integer.toString(num).length(); i++) {
+			sum += Integer.parseInt(Integer.toString(num).charAt(i) + "");
+		}
+		return sum % 2 == 0 ? "Oddish" :"Evenish";
+	}
+
+	public static String reverseOdd(String str) {
+        String[] arr = str.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length() % 2 != 0) {
+                arr[i] = new StringBuilder(arr[i]).reverse().toString();
+            }
+        }
+        return String.join(" ", arr);
+    }
+
+	public static List<String> getHashTags(String str) {
+		String[] arr = str.split(" ");
+		Map<String, Integer> map = new HashMap<>();
+		List<String> result = new ArrayList<>();
+		String longest = "", secondLongest = "", thirdLongest = "";
+
+		for (String word : arr) {
+			word = word.replaceAll("[^a-zA-Z]", "");
+			map.put(word, word.length());
+		}
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			String word = entry.getKey();
+			int length = entry.getValue();
+
+			if (length > longest.length()) {
+				thirdLongest = secondLongest;
+				secondLongest = longest;
+				longest = word;
+			} else if (length > secondLongest.length()) {
+				thirdLongest = secondLongest;
+				secondLongest = word;
+			} else if (length > thirdLongest.length()) {
+				thirdLongest = word;
+			}
+		}
+
+		if (!longest.isEmpty()) result.add(longest);
+		if (!secondLongest.isEmpty()) result.add(secondLongest);
+		if (!thirdLongest.isEmpty()) result.add(thirdLongest);
+
+		return result;
+    }
 }
