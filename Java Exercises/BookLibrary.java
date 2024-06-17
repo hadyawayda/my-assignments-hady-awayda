e3a17c39dcb45b2ca55ab7a7f7bd2d0dce53266f
Hady <hady_awayda@hotmail.com>
2024-06-17 15:09:35 +0300

    Created Book Library

---
+++ Java Exercises/BookLibrary.java
@@ -0,0 +1,135 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class BookLibrary {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Hello there! Enter your choice:");
+            System.out.println("1. Fiction");
+            System.out.println("2. NonFiction");
+            System.out.println("3. List Books");
+            System.out.println("4. Exit");
+            String userInput = scan.nextLine();
+            if ("Exit".toLowerCase().equals(userInput) || "4".equals(userInput)) break;
+            else if ("fiction".equals(userInput.toLowerCase()) || "1".equals(userInput))
+            {
+                System.out.println("Enter title:");
+                String title = scan.nextLine();
+                System.out.println("Enter author:");
+                String author = scan.nextLine();
+                System.out.println("Enter year:");
+                int year = scan.nextInt();
+                scan.nextLine();
+                Fiction fictionBook = new Fiction(title, author, year);
+                Library.addBook(fictionBook);
+
+                System.out.println("Would you like to do more operations? y/n");
+                String choice = scan.nextLine();
+                System.out.println();
+                if ("y".toLowerCase().equals(choice) || "yes".toLowerCase().equals(choice)) continue;
+                else if ("n".toLowerCase().equals(choice) || "no".toLowerCase().equals(choice)) break;
+            }
+            else if ("nonfiction".equals(userInput.toLowerCase()) || "2".equals(userInput))
+            {
+                System.out.println("Enter title:");
+                String title = scan.nextLine();
+                System.out.println("Enter author:");
+                String author = scan.nextLine();
+                System.out.println("Enter year:");
+                int year = scan.nextInt();
+                scan.nextLine();
+                NonFiction nonFictionBook = new NonFiction(title, author, year);
+                Library.addBook(nonFictionBook);
+                System.out.println();
+
+                System.out.println("Would you like to do more operations? y/n");
+                String choice = scan.nextLine();
+                System.out.println();
+                if ("y".toLowerCase().equals(choice) || "yes".toLowerCase().equals(choice)) continue;
+                else if ("n".toLowerCase().equals(choice) || "no".toLowerCase().equals(choice)) break;
+            }
+            else if ("list books".equals(userInput.toLowerCase()) || "3".equals(userInput))
+            {
+                Library.displayBooks();
+                System.out.println();
+
+                System.out.println("Would you like to do more operations? y/n");
+                String choice = scan.nextLine();
+                System.out.println();
+                if ("y".toLowerCase().equals(choice) || "yes".toLowerCase().equals(choice)) continue;
+                else if ("n".toLowerCase().equals(choice) || "no".toLowerCase().equals(choice)) break;
+            }
+            else {
+                System.out.println("Please enter the correct choice. Would you like to try again? y/n");
+                String choice = scan.nextLine();
+                System.out.println();
+                if ("y".toLowerCase().equals(choice) || "yes".toLowerCase().equals(choice)) continue;
+                else if ("n".toLowerCase().equals(choice) || "no".toLowerCase().equals(choice)) break;
+            }
+        }
+    }
+}
+
+abstract class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public abstract String getDetails();
+}
+
+class Fiction extends Book {
+    public Fiction(String title, String author, int year) {
+        super(title, author, year);
+    }
+
+    @Override
+    public String getDetails() {
+        return "Fiction Book - Title: " + getTitle() + ", Author: " + getAuthor() + ", Year: " + getYear();
+    }
+}
+
+class NonFiction extends Book {
+    public NonFiction(String title, String author, int year) {
+        super(title, author, year);
+    }
+
+    @Override
+    public String getDetails() {
+        return "NonFiction Book - Title: " + getTitle() + ", Author: " + getAuthor() + ", Year: " + getYear();
+
+    }
+}
+
+class Library {
+    static ArrayList<Book> books = new ArrayList<>();
+
+    public static void addBook (Book book) {
+        books.add(book);
+    }
+
+    public static void displayBooks() {
+        for (Book book: books) {
+            System.out.println(book.getDetails());
+        }
+    }
+}
